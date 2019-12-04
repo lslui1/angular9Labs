f08ff6e80eb54cb9b1d46323fafebe43aed367ef
Li Lui <llui1@yahoo.com>
2019-12-04 09:11:19 -0500

    more changes

--- my-home-repairs/src/app/app.module.ts
+++ my-home-repairs/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepairListComponent } from './repairs/repair-list/repair-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepairListComponent
   ],
   imports: [
     BrowserModule,
